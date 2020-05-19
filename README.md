3e0744be5bb242f0c787381e80c972a68967cc6a
Igor Rosa (server) <igor.ronan@icloud.com>
2020-05-19 11:43:12 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# MONITORAMENTO DE SERVICOS PYTHON
+
+Monitoramento criado via python com envio de mail via Gmail para serviços que não estao ativos
+
+Para utilizar basta colocar o login e senha de uma conta de email do Gmail e uma conta de email para recebimento.
+
+No servidor que será nonitorado instalar o serviço via crontab abaixo indicando o local do arquivo de configuração;
+
+
+No comando abaixo o monitoramento será rodado a cada 30 minutos
+*/30 * * * * python3 /var/services.py
+
---
+++ services.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+
+
+import smtplib
+import time
+import os
+import configparser
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
+import commands
+
+
+def sendmail(service):
+    #Preparando Email
+    mail_content = '''Ola
+    O servico ''' + service + ''' esta inativo no servidor ''' + commands.getoutput('hostname') + ''';
+    Sera necessario reiniciar o mesmo.
+    Favor nao responder este email.
+
+    Atte.
+    '''
+    sender_address = 'email-for-monitoro@gmail.com'
+    sender_pass = 'xxxxxxxxx'
+    receiver_address='client@seudominio.com'
+    message = MIMEMultipart()
+    message['From'] = sender_address
+    message['To'] = receiver_address
+    message['Subject'] = 'O Servico '+ service +' parado no servidor ' + commands.getoutput('hostname')
+    message.attach(MIMEText(mail_content, 'plain'))
+    session = smtplib.SMTP('smtp.gmail.com', 587)
+    session.starttls()
+    session.login(sender_address, sender_pass)
+    text = message.as_string()
+    session.sendmail(sender_address, receiver_address, text)
+    session.quit()
+    print("Send Alert to service " + service )
+
+
+
+output = commands.getoutput('ps -A')
+if 'cups' not in output:
+    sendmail('cups')
+
+if 'apache2' not in output:
+    sendmail('Apache2')
+
+if 'mysql' not in output:
+    sendmail('Mysql')
